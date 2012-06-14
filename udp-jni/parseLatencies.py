e7a8c7ae2d1145ad08ca9e74388d50a3f7a03128
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-06-14 05:19:25 -0400

    1. Removed project.properties and local.properties since these have to be created by the android tool for each machine anyway. 2. Added parseLatencies.py , a simple Python script to parse latencies from the trace file.

---
+++ udp-jni/parseLatencies.py
@@ -0,0 +1,14 @@
+#! usr/bin/python
+import sys
+FILE=open(sys.argv[1],"r")
+packetLatency=dict()
+minLatency=1000000
+for line in FILE.readlines() :
+      packet=int(line.split()[0])
+      latency=float(line.split()[1])
+      packetLatency[packet]=latency
+      if (latency<minLatency) :
+           minLatency=latency
+
+for packet in packetLatency :
+      print packet,"\t",(packetLatency[packet]-minLatency)*1000
