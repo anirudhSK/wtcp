0c118b0cc213437e60cda74146acd8e2d7b3ccc1
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-07-17 16:53:26 -0400

    1. Fixed a really dumb bug in my code. 2. Added two more kernel variable fixes to donat that fixes the kernel filtering issues.

--- one-way-delay/sender/ethernetToLte.cc
+++ one-way-delay/sender/ethernetToLte.cc
@@ -15,7 +15,7 @@ struct packetstat {
  bool received;
 };
 /* stt is written by the rx, packetstat is written by the sender */
-const int NUM_PACKETS=10000;
+const int NUM_PACKETS=100;
 vector<struct packetstat> packetstats( NUM_PACKETS );
 const double ALPHA=0.125;
 const int TARGET_TT=250000; /* 250 ms or 250000 us */
@@ -31,17 +31,13 @@ Socket::Address getNatAddr( const Socket & sender, const Socket::Address & dest,
   for ( int i = 0; i < 10; i++ ) {
     buf[ i ] = rand() % 256;
   }
-  
   string to_send( buf, 10 );
-
   sender.send( Socket::Packet( dest, to_send ) );
   Socket::Packet received( receiver.recv() );
-
   if ( received.payload != to_send ) {
     fprintf( stderr, "Bad packet received while getting NAT addresses.\n" );
     exit( 1 );
   }
-
   return received.addr;
 }
 /* update smoothed transit time based on new value */ 
@@ -51,53 +47,28 @@ uint64_t updateStt(uint64_t tt, uint64_t stt) {
 /* check for congestion based on rtt and change sleepTime accordingly */
 uint32_t checkCongestion(uint64_t stt,double currentRate) {
    if(stt>TARGET_TT+epsilon) return (currentRate/2); /* Multiplicative decrease */ 
-   else if(stt<TARGET_TT -epsilon) return (currentRate-1); /*Additive increase in some sense */ 
+   else if(stt<TARGET_TT -epsilon) return (currentRate+1); /*Additive increase in some sense */ 
    else return currentRate; /* sit tight */
 }
 /* lteReceiver */
 void* lteReceiver(void* receiverSocket ) {
   Socket lteSocket=*((Socket* )(receiverSocket));
-  struct pollfd pollfds[ 1 ];
-  for ( int i = 0; i < 1; i++ ) {
-    pollfds[ i ].fd = lteSocket.get_sock();
-    pollfds[ i ].events = POLLIN;
-  }
   printf("Created polling file descriptors \n");
   while(1) {
-       /* now, poll */
-
-    if ( poll( pollfds, 2, -1 ) <= 0 ) {
-      perror( "poll" );
-      exit( 1 );
-    }
-   
-    for ( int i = 0; i < 1; i++ ) {
-      //printf("The i value is %d \n",i);
-      if ( pollfds[ i ].revents & POLLERR ) {
-        fprintf( stderr, "Error on LTE %d\n", i );
-        exit( 1 );
-      }
-      if ( pollfds[ i ].revents & POLLIN ) {
-        printf("POLLIN satisfied \n");
-
-        printf("Trying to receive \n");
         Socket::Packet rec = lteSocket.recv();
-        printf("Recieved \n");
         //	assert ( rec.payload.size() == sizeof( packets_sent ) );
         uint64_t receivedTs=rec.timestamp;
         uint64_t seq=*(uint64_t *)rec.payload.data();
-        printf("Transit time of packet %lu is %f milliseconds, sent at %lu us , received at %lu us \n",seq,(double)(receivedTs-packetstats[seq].sent_time)/1000,packetstats[seq].sent_time,receivedTs);
+        printf("Transit time of packet %lu is %f ms,  stt is %f  ms \n",seq,(double)(receivedTs-packetstats[seq].sent_time)/1000000,(double)stt/1000000);
         uint64_t tt=receivedTs-packetstats[seq].sent_time;
         stt=updateStt(tt,stt);
        }
-    return NULL;
-    }
-  }
+  return NULL;
 }
 int main() {
     /* Create and bind Ethernet socket */
     Socket ethernetSocket;
-    Socket::Address ethernetAddress( "18.251.7.128", 9000 );
+    Socket::Address ethernetAddress( "128.30.87.51", 9000 );
     ethernetSocket.bind( ethernetAddress );
     ethernetSocket.bind_to_device( "eth0" );
 
@@ -115,17 +86,18 @@ int main() {
     if(pthread_create(&lteReceiverThread, NULL, &lteReceiver,&lteSocket ))  {
        printf("Could not create receiver thread \n");
     }
-    printf("Just created lteReceiver thread \n");
+//    printf("Just created lteReceiver thread \n");
     /* sending loop ie ethernetSender */
     int numSent =0 ; /* number sent out so far */
-    double currentPacketRate=100; /*current Rate at the sender, start it out at 100 Hz */
+    double currentPacketRate=1.0; /*current Rate at the sender, start it out at 100 Hz */
     while(numSent < NUM_PACKETS) {
        /* check current stt , for instantaneous feedback */
-       currentPacketRate=checkCongestion(stt,currentPacketRate);
+       //currentPacketRate=checkCongestion(stt,currentPacketRate);
        usleep(1000000/currentPacketRate); /*currentRate is in packets per second, usleep takes us */
        char *seq_encoded = (char *)&numSent;
        packetstats[ numSent ].sent_time = Socket::timestamp(); /* maintain state to calc stt */
        ethernetSocket.send(Socket::Packet(lteEndPoint, string( seq_encoded, 51 ) ) );      
+       printf("Current packet rate is %f \n",currentPacketRate);
        numSent++;
     }
 }
