f0405d6ebe4c6f7b07379c031bd3333980aafc2a
anirudh <anirudh@traffic-shaper.(none)>
2012-08-10 12:29:48 -0400

    Added packet filtering before forwarding on both the ingress and egress. Now works end to end (tested by web browsing on client)

--- TrafficShaping/packet-forwader.c
+++ TrafficShaping/packet-forwader.c
@@ -8,6 +8,13 @@
 #include <string.h>          // strcpy, memset(), and memcpy()
 #include <net/if.h>          // struct ifreq
 #include <sys/ioctl.h>       // for SIOCGIFINDEX
+#include <assert.h>
+char client_mac[6]={0x00,0x22,0x68,0x1c,0xa6,0x22};
+char bcast[6]={0xff,0xff,0xff,0xff,0xff,0xff};
+int check_mac_addr(char m1[6],char m2[6]) {
+       /* check if two mac addr match */
+      return ((m1[0]==m2[0])&&(m1[1]==m2[1])&&(m1[2]==m2[2])&&(m1[3]==m2[3])&&(m1[4]==m2[4])&&(m1[5]==m2[5])) ;
+}
 
 void bind_to_if(int socket,char* if_name) {
   /* Bind to interface, 
@@ -40,13 +47,19 @@ int main(int argc,char** argv) {
   /* command line handling */
   char ingress[10];
   char egress[10];
-  if(argc < 3)  {
-     printf("Usage: packet-forwarder ingress-interface egress-interface \n");
+  char client_position[15];
+  if(argc < 4)  {
+     printf("Usage: packet-forwarder ingress-interface egress-interface client-position \nSpecify behind-egress or behing-ingress fo client position \n");
      exit(EXIT_FAILURE);
   }
   else {
      strcpy(ingress,argv[1]);
      strcpy(egress,argv[2]);
+     strcpy(client_position,argv[3]);
+     if((!strcmp(client_position,"behind_ingress")==0 ) && (!strcmp(client_position,"behind_egress")==0)) {
+       printf("invalid position \n");
+       exit(EXIT_FAILURE);
+     }
   }
     
   /* variable decl */
@@ -68,7 +81,7 @@ int main(int argc,char** argv) {
     exit (EXIT_FAILURE);
   }
 
-  /* bind to interface eth0 */
+  /* bind to ingress */
   bind_to_if(recv_socket,ingress); 
 
   /* Create another packet socket to send packets to. In some sense we are a dumb software repeater */
@@ -77,7 +90,7 @@ int main(int argc,char** argv) {
     exit (EXIT_FAILURE);
   }
 
-  /* bind to interface eth1 */
+  /* bind to egress */
   bind_to_if(send_socket,egress); 
 
   /* receive in a tight loop, use MSG_TRUNC to get actual msg length */
@@ -91,16 +104,33 @@ int main(int argc,char** argv) {
           exit (EXIT_FAILURE);
         }
     }
-    else { 
-          printf("Packet received with %d bytes  \n",recv_bytes);
+    /* parse src and dst mac */
+    char* dst_mac=(char*)(ether_frame);
+    char* src_mac=(char*)(ether_frame +6); 
+   
+    /* if client behind egress  : forward packets to   it only if they are broadcast or destined to client */
+    if(strcmp("behind_egress",client_position)==0) {
+         if ( check_mac_addr(dst_mac,client_mac) || check_mac_addr(dst_mac,bcast) )   {
+             if ((sent_bytes = send(send_socket,ether_frame,recv_bytes,MSG_TRUNC))<0) {
+               perror("send() failed:");
+               exit(EXIT_FAILURE);
+             }
+         }
     }
-    if ((sent_bytes = send(send_socket,ether_frame,recv_bytes,MSG_TRUNC))<0) {
-         perror("send() failed:");
-         exit(EXIT_FAILURE);
+    /* if client behind ingress : forward packets from it only if they are from the client.                */
+    else if (strcmp("behind_ingress",client_position)==0) {
+         if (  check_mac_addr(src_mac,client_mac) )   {
+             if ((sent_bytes = send(send_socket,ether_frame,recv_bytes,MSG_TRUNC))<0) {
+               perror("send() failed:");
+               exit(EXIT_FAILURE);
+             }
+         }
     }
-    else {
-         printf("Sent out %d bytes \n",sent_bytes);
+    else  {
+      assert(0); // can't come here 
     }
   }
   return 0;
 }
+
+
