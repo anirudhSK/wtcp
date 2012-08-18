df6fea83f7f450459c3502ec3c42a900a6e4aa4a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-08-18 10:28:47 -0400

    Cmd line arguments.

--- signal-gen/tone-gen.cc
+++ signal-gen/tone-gen.cc
@@ -1,16 +1,66 @@
 #include<iostream>
 #include"tone.hh"
 #include"awgn.hh"
+#include<getopt.h>
 using namespace std;
 
 
+const float SAMPLE_RATE=44100;
+int main(int argc,char ** argv) {
 
-int main() {
+   int center_freq = 0;
+   int bandwidth   = 0;
+   float duration  = 0; 
+   float amplitude = 0 ; 
+
+   static struct option long_options[] = {
+    /* These options don't set a flag.
+       We distinguish them by their indices. */
+    {"center_freq",required_argument, 0, 'c'},
+    {"bandwidth"  ,required_argument, 0, 'b'},
+    {"duration"   ,required_argument, 0, 'd'},
+    {"amplitude"  ,required_argument, 0, 'a'},
+    {0, 0, 0, 0}
+   };
+   /* getopt_long stores the option index here. */
+   int option_index = 0;
+   int c=0;
+   while ((c=getopt_long (argc, argv, "c:b:d:a:",long_options, &option_index)) != -1) {
+     
+     switch (c) {
+       case 'c':
+         center_freq=atoi(optarg);
+         break;
+     
+       case 'b':
+         bandwidth=atoi(optarg);
+         break;
+     
+       case 'd':
+         duration=atof(optarg);
+         break;
+     
+       case 'a':
+         amplitude=atof(optarg);
+         break;
+       
+       default:
+         printf("Usage : ./tonegen --center_freq F --bandwidth B --duration D --amplitude A \n");
+         exit(1);
+     }
+   }
+
+   /* Print usage  */
+   if (center_freq==0 || bandwidth == 0 || duration == 0 || amplitude == 0) {
+         printf("Invalid or incomplete arguments\nUsage : ./tonegen --center_freq F --bandwidth B --duration D --amplitude A \n");
+         exit(1);
+   }
+
    /* generate 2000 Hz tone wave */ 
    /* for 0.3 seconds */ 
    /* at 44.1 KHZ*/
    /* Amplitude of 0.9 to avoid saturation */  
-   Tone test_tone(2000,0.3,44100,0.9); 
+   Tone test_tone(center_freq,duration,SAMPLE_RATE,amplitude); 
 //   test_tone.to_file("output.dat"); 
 
    /* Test case 2 : AWGN noise, no interpolation  */
@@ -28,7 +78,7 @@ int main() {
    /* Sample_rate of 44.1KHZ */
    /* amplitude of 0.9  */
    
-   AwgnNoise white_noise2(100,0.3,44100,0.9); 
+   AwgnNoise white_noise2(bandwidth,duration,SAMPLE_RATE,amplitude); 
    white_noise2.to_file("noise.dat");
 
    /* Test case 4: Shift noise to elsewhere on the spectrum */ 
@@ -37,11 +87,11 @@ int main() {
    double* noise=white_noise2.get_noise();  
     
    /* multiply them with tone to 'shift' them */
-   double *shifted_samples=test_tone.multiply(noise,44100*0.3); 
+   double *shifted_samples=test_tone.multiply(noise,(long int)SAMPLE_RATE*(duration)); 
 
    /* create a new AwgnNoise wrapper */
    AwgnNoise shifted_noise(white_noise2,shifted_samples);
    
-   /* dumpo new samples */ 
+   /* dump new samples */ 
    shifted_noise.to_file("noiseShifted.dat");
 }
