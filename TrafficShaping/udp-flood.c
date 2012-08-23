96da239f69bef3e5c7c658d0f663dd9a850a2b17
anirudh <anirudh@traffic-shaper.(none)>
2012-08-23 18:05:59 -0400

    Checked how much throughout udp-flood was actually generating by adding byte counters. It's around 30 MBytes per sec. Wonder why the traffic shaper shows only 17MBytes per sec when shaping this udp-flood traffic, despite not having any queue.

--- TrafficShaping/udp-flood.c
+++ TrafficShaping/udp-flood.c
@@ -10,6 +10,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include<signal.h>
 struct senderdata {
   int datagram_count;
   int queue_len;
@@ -17,7 +18,16 @@ struct senderdata {
   int us;
   int padding;
 };
-
+uint64_t ts_start=0;
+uint64_t total_count=0;
+void my_handler (int sig)
+{
+ struct timeval ts;
+ gettimeofday(&ts,NULL);
+ uint64_t now=ts.tv_sec*1e6+ts.tv_usec;
+ printf("Transfer rate is %f MBytes per sec \n", (float)total_count/(float)(now-ts_start));
+ exit(1);
+}
 int main( int argc, char *argv[] )
 {
   int port = 9999;
@@ -46,19 +56,26 @@ int main( int argc, char *argv[] )
   if ( !inet_aton("220.181.111.147", &addr.sin_addr ) ) {
     exit( 1 );
   }
+  signal(SIGINT, my_handler);
 
   struct senderdata data;
   data.datagram_count = 10;
   data.queue_len = 10;
   data.secs = 5;
   data.us = 10;
-
+  struct timeval ts;
+  total_count=0;
+  gettimeofday(&ts,NULL);
+  ts_start=ts.tv_sec*1e6 + ts.tv_usec;
   while ( 1 ) {
     /* send datagrams to target */
         if ( (sendto( sock, &data, sizeof( data ),
 		 0, (struct sockaddr *)&addr, sizeof(struct sockaddr_in) )) < 0 ) {
       perror( "sendto" );
       exit( 1 );
+    }
+    else  {
+     total_count=total_count+62;
     }
   }
   return 0;
