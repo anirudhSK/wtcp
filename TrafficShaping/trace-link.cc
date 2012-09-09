3eac8c0535e79b4ae547798983ea8a0c8dfcc848
anirudh <anirudh@Sim-Box.(none)>
2012-09-09 18:46:21 -0400

    Spreads out trace bytes to reduce burstiness.

--- TrafficShaping/trace-link.cc
+++ TrafficShaping/trace-link.cc
@@ -3,9 +3,8 @@ TraceLink::TraceLink(int fd,bool t_output_enable,std::string t_link_name,std::st
    Link(fd,t_output_enable,t_link_name) ,
    last_token_update(begin_time),
    token_count(0),
-   BURST_SIZE(1600), /* 1 packet */
-   pkt_schedule(PktSchedule (trace_file)) {
-   token_count=pkt_schedule.current_byte_credit;
+   delivery_schedule(DeliverySchedule (trace_file)) {
+   token_count=delivery_schedule.current_byte_credit;
    std::cout<<"Starting out with byte credit "<<token_count<<" at time "<<begin_time<<"\n"; 
 }
 
@@ -15,9 +14,9 @@ int TraceLink::recv(uint8_t* ether_frame,uint16_t size,uint64_t rx_ts) {
 }
 
 void TraceLink::tick() {
-   uint64_t ts_now=Link::timestamp(); 
+   uint64_t ts_now=Link::timestamp();
    Link::print_stats(ts_now);
-   update_token_count(ts_now,0);
+   update_token_count(ts_now);
    /* Can I send pkts right away ? */ 
    if(!pkt_queue.empty()) {
      Payload head=pkt_queue.front();
@@ -37,19 +36,19 @@ void TraceLink::tick() {
    }
 }
 
-void TraceLink::update_token_count(uint64_t current_ts,long double drain) {
+void TraceLink::update_token_count(uint64_t current_ts) {
      /* check if you see a more recent line */
-     uint32_t current_byte_credit=0 ; /* If there is a new point in the file , then update this */ 
-     while((current_ts-begin_time)>=pkt_schedule.next_timestamp) {  
-        assert((!pkt_schedule.pkt_list.empty())); 
-        current_byte_credit += std::get<1>(pkt_schedule.pkt_list.front()); 
+     double current_byte_credit=0 ; /* If there is a new point in the file , then update this */ 
+     while((current_ts-begin_time)>=delivery_schedule.next_timestamp) {  
+        assert((!delivery_schedule.pkt_delivery_list.empty())); 
+        current_byte_credit += std::get<1>(delivery_schedule.pkt_delivery_list.front()); 
       #ifdef DEBUG
         std::cout<<link_name<<" Added credit of "<<current_byte_credit<<" bytes at time "<<current_ts<<" \n";
       #endif
         /* House keeping on pkt schedule */
-        pkt_schedule.pkt_list.pop_front(); 
-        if(!pkt_schedule.pkt_list.empty()) {
-          pkt_schedule.next_timestamp=std::get<0>(pkt_schedule.pkt_list.front());
+        delivery_schedule.pkt_delivery_list.pop_front(); 
+        if(!delivery_schedule.pkt_delivery_list.empty()) {
+          delivery_schedule.next_timestamp=std::get<0>(delivery_schedule.pkt_delivery_list.front());
         }
         else {
           std::cout<<link_name<<" bad luck, no more credit left, kill simulation \n";
@@ -57,17 +56,17 @@ void TraceLink::update_token_count(uint64_t current_ts,long double drain) {
         }
      }
      /* new token count in bytes */
-     long double new_token_count=token_count+current_byte_credit-drain; 
+     long double new_token_count=token_count+current_byte_credit; 
      last_token_update=current_ts;
      token_count=(new_token_count);
 }
 
-TraceLink::PktSchedule::PktSchedule(std::string t_file_name) :
+TraceLink::DeliverySchedule::DeliverySchedule(std::string t_file_name) :
      file_name(t_file_name) ,
      next_timestamp(-1),
      current_byte_credit(0),
-     pkt_list(std::list<std::tuple<uint64_t,uint32_t>>()) {
-     /* populate pkt_list using the file */
+     pkt_delivery_list(std::list<std::tuple<uint64_t,double>>()) {
+     /* populate pkt_delivery_list using the file */
      std::ifstream pkt_stream (file_name.c_str());
      uint64_t time;
      uint32_t bytes;
@@ -80,28 +79,82 @@ TraceLink::PktSchedule::PktSchedule(std::string t_file_name) :
        pkt_stream>>time>>bytes;
        if( pkt_stream.eof() ) break;
        if(time==prev_time) {
-         std::tuple<uint64_t,uint32_t> front=pkt_list.back();
+         std::tuple<uint64_t,double> front=pkt_delivery_list.back();
          uint32_t stored_credit=std::get<1>(front);
          uint32_t new_credit=stored_credit+bytes;
-         pkt_list.pop_back(); /* remove old credit */
-         pkt_list.push_back(std::tuple<uint64_t,uint32_t>(time*1e6,new_credit)); /* push agg. credit */ 
+         pkt_delivery_list.pop_back(); /* remove old credit */
+         pkt_delivery_list.push_back(std::tuple<uint64_t,double>(time*1e6,new_credit)); /* push agg. credit */ 
        }
        else {
-        pkt_list.push_back(std::tuple<uint64_t,uint32_t>(time*1e6,bytes)); /* time in ns, trace comes in in ms */ 
+        pkt_delivery_list.push_back(std::tuple<uint64_t,double>(time*1e6,bytes)); /* time in ns, trace comes in in ms */ 
        }
        prev_time=time;
      }
-     assert(pkt_list.size()>=2);
-     current_byte_credit=std::get<1>(pkt_list.front());
-     pkt_list.pop_front();
-     next_timestamp=std::get<0>(pkt_list.front());
+     /* before interpolation */
+     FILE* fh=fopen((file_name+"pre-interp").c_str(),"w");
+     dump_schedule(fh);
+     interpolate();
+     fh=fopen((file_name+"post-interp").c_str(),"w");
+     dump_schedule(fh);
+     
+     assert(pkt_delivery_list.size()>=2);
+     current_byte_credit=std::get<1>(pkt_delivery_list.front());
+     pkt_delivery_list.pop_front();
+     next_timestamp=std::get<0>(pkt_delivery_list.front());
+}
+void TraceLink::DeliverySchedule::dump_schedule(FILE* fh) {
+    for (std::list<std::tuple<uint64_t,double>>::iterator it = pkt_delivery_list.begin(); it != pkt_delivery_list.end(); it++) {
+        std::tuple<uint64_t,double> current=*it;
+        double current_bytes=std::get<1>(current);
+        uint64_t current_time=std::get<0>(current);
+        fprintf(fh,"%ld \t %.5f \n",current_time,current_bytes);
+    }
+}
+void TraceLink::DeliverySchedule::interpolate() {
+    /* Spread out credits to make sure there are some credits every millisecond 
+       This will reduce burstiness */
+    std::list<std::tuple<uint64_t,double>>::iterator it = pkt_delivery_list.begin();
+    while ( it != pkt_delivery_list.end() ) {
+        std::tuple<uint64_t,double> current=*it;
+        double current_bytes=std::get<1>(current);
+        uint64_t current_time=std::get<0>(current);
+        /* fetch the next packet delivery */
+        it++;
+        if(it == pkt_delivery_list.end()) {
+          /* end of packet delivery trace */
+          break;
+        }
+        uint64_t next_time=std::get<0>(*it);
+        uint64_t delta_in_ms=(uint64_t)((next_time-current_time)/1e6);
+        assert(delta_in_ms>=1); 
+        
+        if(delta_in_ms==1) { 
+          continue;
+        }
+        else if(delta_in_ms > 100) {
+          continue; /* too long must be an outage, no point interpolating */
+        }
+        else  { 
+          /* Go back to the credit you want to spread out */
+          it--;
+          /* Erase it and move to the next credit */
+          it=pkt_delivery_list.erase(it);
+          /* Now, spread the bytes evenly */ 
+          double bytes_per_ms = current_bytes/delta_in_ms;
+          uint16_t i=0;
+          for ( i=0 ; i<delta_in_ms ; i++ ) {
+             std::tuple<uint64_t,double> interp_credit(current_time+i*1e6,bytes_per_ms);
+             pkt_delivery_list.insert(it,interp_credit);
+          }
+        }
+    }
 }
 
 uint64_t TraceLink::calc_next_time(uint32_t head_size) {
      long double required_tokens=head_size-token_count;
-      /* Run through the pkt_list to check how long before you accumulate sufficient tokens */ 
+      /* Run through the pkt_delivery_list to check how long before you accumulate sufficient tokens */ 
      long double acc=0; 
-     for (std::list<std::tuple<uint64_t,uint32_t>>::iterator it = pkt_schedule.pkt_list.begin(); it != pkt_schedule.pkt_list.end(); it++) {
+     for (std::list<std::tuple<uint64_t,double>>::iterator it = delivery_schedule.pkt_delivery_list.begin(); it != delivery_schedule.pkt_delivery_list.end(); it++) {
         std::tuple<uint64_t,uint32_t> current=*it;
         acc=acc+std::get<1>(current);
         if(acc>=required_tokens) {
--- TrafficShaping/trace-link.hh
+++ TrafficShaping/trace-link.hh
@@ -2,14 +2,14 @@
 #ifndef TRACE_LINK_HH
 #define TRACE_LINK_HH
 #include "link.hh"
+#include <iostream>
 #include <queue>             
 class TraceLink: public Link {
    private : 
     uint64_t last_token_update;    
     /* time at which token count was last updated */
     long double token_count ;        
-    const uint32_t BURST_SIZE;
-    class PktSchedule {
+    class DeliverySchedule {
      /* a schedule of packet arrivals for the experiment. 
           trace driven */
      private :
@@ -18,18 +18,21 @@ class TraceLink: public Link {
          uint64_t next_timestamp;
          uint32_t current_byte_credit; 
          /* list of nanosec,byteCounter tuples */ 
-         std::list <std::tuple<uint64_t,uint32_t>> pkt_list; 
-         PktSchedule(std::string file_name) ;  
+         std::list <std::tuple<uint64_t,double>> pkt_delivery_list; 
+         DeliverySchedule(std::string file_name) ;  
+         void dump_schedule(FILE* fh);
+         void interpolate() ;
      };
-     PktSchedule pkt_schedule;
+     DeliverySchedule delivery_schedule;
  public : 
  
     virtual void tick() ;          
     /*virtual cause this was taken from Link*/
     virtual int recv(uint8_t* ether_frame,uint16_t size,uint64_t rx_timestamp) ;
     /* Same here */
-    void update_token_count(uint64_t current_ts,long double drained); 
-     TraceLink(int fd,bool output_enable,std::string link_name,std::string trace_file);
+    void update_token_count(uint64_t current_ts); 
+    TraceLink(int fd,bool output_enable,std::string link_name,std::string trace_file);
     uint64_t calc_next_time(uint32_t head_size);
+
 };
 #endif
